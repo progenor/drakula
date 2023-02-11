30b9e64061a9ba50c5c982a376f2c36af0e2fc7f
progenor <nero6126@gmail.com>
2023-02-11 20:02:35 +0200

    jancsi_drakulaja

---
+++ drakula-lista.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Drakula Lista</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+      <h1 style="font-size: 35px">Drakula</h1>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="/drakula-lista.php">More</a></li>
+      </ul>
+    </nav>
+    <div class="main_div">
+      <main>
+        <h1>Drakulak listaja</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptates, quod, quia, voluptatibus quae voluptatem quidem
+        </p>
+        <div class="theList">
+          <ol>
+            <?php
+              // irasd ki a drakulakat
+              
+
+
+            ?>
+          </ol>
+        </div>
+      </main>
+    </div>
+    <footer>
+      <p>Beres Janos</p>
+    </footer>
+  </body>
+</html>
